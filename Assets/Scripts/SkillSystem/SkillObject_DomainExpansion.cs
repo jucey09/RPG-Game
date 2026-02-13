e62c2b08a59e58a86aaa8212ae2b85658ce88efb
jucey09 <powellcaleb09@gmail.com>
2026-02-13 12:11:24 -0500

    working on the domain expansion

---
+++ Assets/Scripts/SkillSystem/SkillObject_DomainExpansion.cs
@@ -0,0 +1,69 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SkillObject_DomainExpansion : SkillObject_Base
+{
+    private Skill_DomainExpansion domainManager;
+
+    private float maxSize = 10;
+    private float duration;
+    private float expandSpeed = 2;
+    private float slowDownPercent = 0.9f;
+    private Vector3 targetScale;
+    private bool isShrinking;
+
+
+    private void Update()
+    {
+        HandleScaling();
+    }
+    public void SetupDomain(Skill_DomainExpansion domainManager)
+    {
+        this.domainManager = domainManager;
+
+        duration = domainManager.GetDomainDuration();
+        slowDownPercent = domainManager.GetSlowPercentage();
+        expandSpeed = domainManager.expandSpeed;
+        float maxSize = domainManager.maxDomainSize;
+
+        targetScale = Vector3.one * maxSize;
+        Invoke(nameof(ShrinkDomain), duration);
+    }
+
+    private void HandleScaling()
+    {
+        float sizeDiffrence = Mathf.Abs(transform.localScale.x - targetScale.x);
+        bool shouldChangeScale = sizeDiffrence > 0.1f;
+
+        if(shouldChangeScale)
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, expandSpeed * Time.deltaTime);
+
+        if(isShrinking && sizeDiffrence < 0.1f)
+            Destroy(gameObject);
+    }
+
+    private void ShrinkDomain()
+    {
+        targetScale = Vector3.zero;
+        isShrinking = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.GetComponent<Enemy>();
+
+        if (enemy != null)
+            return;
+        
+        enemy.SlowDownEntity(duration, slowDownPercent);
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Enemy enemy = collision.GetComponent<Enemy>();
+
+        if (enemy != null)
+            return;
+
+    }
+}
--- Assets/Scripts/SkillSystem/Skill_DomainExpansion.cs
+++ Assets/Scripts/SkillSystem/Skill_DomainExpansion.cs
@@ -2,6 +2,36 @@ using UnityEngine;
 
 public class Skill_DomainExpansion : Skill_Base
 {
+    [SerializeField] private GameObject domainPrefab;
+
+    [Header("Slowing Down Upgrade")]
+    [SerializeField] private float slowDownPercent = 0.8f;
+    [SerializeField] private float slowDownDomainDuration = 5;
+
+    [Header("Spell Casting Upgrade")]
+    [SerializeField] private float spellcastingDomainSlowDowm = 1;
+    [SerializeField] private float spellCastingDomainDuration = 8;
+
+    [Header("Domain Details")]
+    public float maxDomainSize = 10;
+    public float expandSpeed = 2;
+
+    public float GetDomainDuration()
+    {
+        if (upgradeType == SkillUpgradeType.Domain_SlowingDown)
+            return slowDownDomainDuration;
+        else
+            return spellCastingDomainDuration;
+    }
+
+    public float GetSlowPercentage()
+    {
+        if(upgradeType == SkillUpgradeType.Domain_SlowingDown)
+            return slowDownPercent;
+        else
+            return spellcastingDomainSlowDowm;
+    }
+
     public bool InstantDomain()
     {
         return upgradeType != SkillUpgradeType.Domain_EchoSpam
@@ -9,6 +39,7 @@ public class Skill_DomainExpansion : Skill_Base
     }
     public void CreateDomain()
     {
-        Debug.Log("Domain Created");
+        GameObject domain = Instantiate(domainPrefab, transform.position, Quaternion.identity);
+        domain.GetComponent<SkillObject_DomainExpansion>().SetupDomain(this);
     }
 }
